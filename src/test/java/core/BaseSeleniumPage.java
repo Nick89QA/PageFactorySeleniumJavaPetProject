cb95e5200029676712ad1fa51b959751637855e3
Nickolay Petrych <npetrych@alfabank.ru>
2023-06-09 11:41:46 +0100

    add search method

--- src/test/java/core/BaseSeleniumPage.java
+++ src/test/java/core/BaseSeleniumPage.java
@@ -1,6 +1,7 @@
 package core;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 abstract public class BaseSeleniumPage {
     protected static WebDriver driver;
---
+++ src/test/java/helpers/Helper.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//public class Helper {
+//
+//    public
+//    WebDriverWait wait = new WebDriverWait(driver, 4);
+//
+//
+//    JavascriptExecutor js = (JavascriptExecutor) driver;
+//   }
+//
+//
+//
+//
+//
+// }
