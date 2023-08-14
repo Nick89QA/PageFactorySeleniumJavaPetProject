c89595137918dcab331bd9c5e93bb6a22f599c36
Nickolay Petrych <npetrych@alfabank.ru>
2023-08-14 10:22:27 +0100

    add groovy script

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,1 +1,14 @@
 task_branch = "${TEST_BRANCH_NAME}"
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
+base_git_url = "https://github.com/Nick89QA/PetProjectSelenium"
+
+
+node {
+    stage ('Build') {
+        git url: 'https://github.com/cyrille-leclerc/multi-module-maven-project'
+        withMaven {
+            sh "mvn clean verify "
+        } 
+    }
+}
